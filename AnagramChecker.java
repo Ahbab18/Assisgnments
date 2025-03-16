d2993c765e92436f5df43a65f29c0207c0b75807
Ahbab Ahmad <141326951+Ahbab18@users.noreply.github.com>
2025-03-16 12:59:20 +0530

    Update AnagramChecker.java

--- AnagramChecker.java
+++ AnagramChecker.java
@@ -3,22 +3,19 @@ import java.util.Scanner;
 
 public class AnagramChecker {
     public static boolean isAnagram(String str1, String str2) {
-        // Remove spaces and convert to lowercase for uniformity
+        
         str1 = str1.replaceAll("\\s", "").toLowerCase();
         str2 = str2.replaceAll("\\s", "").toLowerCase();
-        
-        // If lengths are not equal, they can't be anagrams
+    
         if (str1.length() != str2.length()) {
             return false;
         }
         
-        // Convert strings to character arrays and sort them
         char[] charArray1 = str1.toCharArray();
         char[] charArray2 = str2.toCharArray();
         Arrays.sort(charArray1);
         Arrays.sort(charArray2);
         
-        // Compare sorted arrays
         return Arrays.equals(charArray1, charArray2);
     }
 
